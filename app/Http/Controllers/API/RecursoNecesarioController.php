55e6b393c0868ef86fb4ccac4c83ce08c9e5be02
issalazar00 <is.salazar00@gmail.com>
2021-06-21 17:42:04 -0500

    Correcion 2 guardado de alimentos por siembra

--- app/Http/Controllers/API/RecursoNecesarioController.php
+++ app/Http/Controllers/API/RecursoNecesarioController.php
@@ -196,15 +196,28 @@ class RecursoNecesarioController extends Controller
         $recursoNecesario->save();
 
         if($request['tipo_actividad'] == '1') {
-					foreach ($request->id_siembra as $siembra){
-						$siembras = Siembra::findOrFail($siembra);
-						$siembras->fecha_alimento = $request['fecha_ra'];
-						$siembras->save();
-	
-						$recursoSiembra = new RecursoSiembra();
-						$recursoSiembra->id_registro = $recursoNecesario->id;
-						$recursoSiembra->id_siembra =$siembra;
-						$recursoSiembra->save();
+					if(!is_array($request['id_siembra'])) {
+						$siembras = Siembra::findOrFail($request['id_siembra']);
+					$siembras->fecha_alimento = $request['fecha_ra'];
+					$siembras->save();
+
+					$recursoSiembra = new RecursoSiembra();
+					$recursoSiembra->id_registro = $recursoNecesario->id;
+					$recursoSiembra->id_siembra =$request['id_siembra'];
+					$recursoSiembra->save();
+					}
+
+					else {
+						foreach ($request->id_siembra as $siembra){
+							$siembras = Siembra::findOrFail($siembra);
+							$siembras->fecha_alimento = $request['fecha_ra'];
+							$siembras->save();
+		
+							$recursoSiembra = new RecursoSiembra();
+							$recursoSiembra->id_registro = $recursoNecesario->id;
+							$recursoSiembra->id_siembra =$siembra;
+							$recursoSiembra->save();
+						}
 					}
         }else{
 					foreach ($request->id_siembra as $siembra) {
